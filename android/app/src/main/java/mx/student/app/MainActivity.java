a0d34223a766d120c3b341e208cf1c37ee3f2e01
Alejandro Calzada <alejandro.calzada.algravez@gmail.com>
2025-08-27 16:17:33 -0600

    add android and ios libraries

---
+++ android/app/src/main/java/mx/student/app/MainActivity.java
@@ -0,0 +1,5 @@
+package mx.student.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'mx.student.app',
+  appName: 'student-tools',
+  webDir: 'build'
+};
+
+export default config;
