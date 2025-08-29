c859aa5c746fb2fba76f7e82c8a1c810c3abba3b
Alejandro Calzada <alejandro.calzada.algravez@gmail.com>
2025-08-28 18:45:17 -0600

    add splash screen and icon

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,15 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'mx.student.app',
   appName: 'student-tools',
-  webDir: 'build'
+  webDir: 'build',
+  plugins: {
+    SplashScreen: {
+      launchShowDuration: 3000,
+      backgroundColor: '#ffffff',
+      splashFullScreen: true,
+      splashImmersive: false
+    }
+  }
 };
 
 export default config;
