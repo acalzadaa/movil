56dc9381771b3144acb5d40deca7a308c77bad2e
Alejandro Calzada <alejandro.calzada.algravez@gmail.com>
2025-08-29 10:19:25 -0600

    ajuste de datos dummy

--- src/lib/asistenciasData.ts
+++ src/lib/asistenciasData.ts
@@ -1,22 +1,22 @@
 const asistenciasData = [
   {
     id: "1",
-    name: "Yoga Matutino",
-    location: "Estudio Central",
+    name: "Taller de Cirugia",
+    location: "Hospital U.M.F 33",
     time: "08:00 AM",
     signed: false
   },
   {
     id: "2",
-    name: "Pilates Avanzado",
-    location: "Gimnasio Norte",
+    name: "Taller Practico de Traumatologia",
+    location: "Hospital U.M.F 12",
     time: "10:00 AM",
     signed: false
   },
   {
     id: "3",
-    name: "Zumba",
-    location: "Sala Este",
+    name: "Taller Practico de Cardiologia",
+    location: "Hospital Cardiologia",
     time: "06:00 PM",
     signed: false
   }
